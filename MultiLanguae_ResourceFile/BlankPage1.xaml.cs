2023b86b40a1b3a2b234d8817b2f030f1247f1ed
DangLinh92 <hoamoclan.3192@gmail.com>
2020-02-08 10:38:55 +0700

    commit multilanguage

--- MultiLanguae_ResourceFile/BlankPage1.xaml.cs
+++ MultiLanguae_ResourceFile/BlankPage1.xaml.cs
@@ -13,29 +13,26 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace MultiLanguae_ResourceFile
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class BlankPage1 : Page
     {
-        public MainPage()
+        public BlankPage1()
         {
             this.InitializeComponent();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Frame.Navigate(typeof(BlankPage1));
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            var resourceLoader = Windows.ApplicationModel.Resources.ResourceLoader.GetForCurrentView("ErrorMessages");
-            this.txtErr.Text = resourceLoader.GetString("ErrorMsg");
+            if (Frame.CanGoBack)
+            {
+                Frame.GoBack();
+            }
         }
     }
 }
